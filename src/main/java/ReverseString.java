38406a83136093ca6d740f275d48a96b6793e6bf
FexMass <fertz1993@gmail.com>
2021-12-26 17:28:42 +0100

    Reverse string task. Tests added

--- src/main/java/ReverseString.java
+++ src/main/java/ReverseString.java
@@ -1,2 +1,47 @@
+/**
+ * String reverse
+ */
 public class ReverseString {
+
+    /**
+     * Method for reversing String via loop
+     * @param input given String
+     * @return reversed String
+     */
+    public String reverseStringWithRegularLoop(String input) {
+        if (checkIfStringIsOk(input)) {
+            return input;
+        }
+
+        //immutable object, while regular String is immutable
+        StringBuilder output = new StringBuilder();
+
+        for (int i = input.length() - 1; i >= 0 ; i--) {
+            output.append(input.charAt(i));
+        }
+
+        return output.toString();
+    }
+
+    /**
+     * Method for reversing String via recursion
+     * @param input given String
+     * @return reversed String
+     */
+    public String reverseStringWithRecursion(String input) {
+        if (checkIfStringIsOk(input)) {
+            return input;
+        }
+
+        return reverseStringWithRecursion(input.substring(1)) + input.charAt(0);
+    }
+
+    /**
+     * Checker method if String is null or less size than 2 character
+     * @param input given String
+     * @return true if its null or less than 2
+     */
+    public boolean checkIfStringIsOk(String input) {
+        return input == null || input.length() <= 1;
+    }
 }
--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,2 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TestCases {
+
+    private ReverseString reverseString;
+
+    @Before
+    public void init() {
+        reverseString = new ReverseString();
+    }
+
+    @Test
+    public void reverseStringWithRegularLoop() {
+        String input = "bbaa";
+        String input1 = "ab ab";
+        String input2 = "massimo";
+        String input3 = "";
+
+        assertEquals("aabb", reverseString.reverseStringWithRegularLoop(input));
+        assertEquals("ba ba", reverseString.reverseStringWithRegularLoop(input1));
+        assertEquals("omissam", reverseString.reverseStringWithRegularLoop(input2));
+        assertEquals("", reverseString.reverseStringWithRegularLoop(input3));
+        assertNull(reverseString.reverseStringWithRegularLoop(null));
+    }
+
+    @Test
+    public void reverseStringWithRecursion() {
+        String input = "bbaa";
+        String input1 = "ab ab";
+        String input2 = "massimo";
+        String input3 = "";
+
+        assertEquals("aabb", reverseString.reverseStringWithRecursion(input));
+        assertEquals("ba ba", reverseString.reverseStringWithRecursion(input1));
+        assertEquals("omissam", reverseString.reverseStringWithRecursion(input2));
+        assertEquals("", reverseString.reverseStringWithRecursion(input3));
+        assertNull(reverseString.reverseStringWithRecursion(null));
+
+    }
 }
